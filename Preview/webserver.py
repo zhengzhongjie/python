961a3bb3b279e60cc386819a12ff970efa1d1ff6
zhengzhongjie <zhengzhongjie@aodiansoft.com>
2015-05-18 23:22:52 +0800

    A web interface.

--- Preview/webserver.py
+++ Preview/webserver.py
@@ -1,9 +1,9 @@
+#!/usr/local/bin/python3
 """
 Implement an HTTP web server in Python that knows how to run server-side
 CGI scripts coded in Python; serves files and scripts from current working
 dit; Python scripts must be stored in webdir\cgi-bin or webdir\htbin;
 """
-#!/usr/local/bin/python3
 import os, sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
