047f79665fe3efc268c0a4fb8f61c74fa92dff5a
zhongjiezheng <zzjie1991@gmail.com>
2015-05-24 12:16:13 +0800

    _thread module

---
+++ System/Threads/thread-count-wait3.py
@@ -0,0 +1,22 @@
+"""
+passed in mutex object shared by all threads instead of globals;
+use with coutext manager statement fot auto acquire/release;
+sleep calls added to acoid busy loops annd simulate real work
+"""
+import _thread as thread, time
+stdoutmutex = thread.allocate_lock()
+numthreads  = 5
+exitmutexes = [thread.allocate_lock() for i in range(numthreads)]
+
+def counter(myId, count, mutex):
+	for i in range(count):
+		time.sleep(1 / (myId+1))
+		with mutex:
+			print('[%s] => %s' % (myId, i))
+	exitmutexes[myId].acquire()
+
+for i in range(numthreads):
+	thread.start_new_thread(counter, (i, 5, stdoutmutex))
+
+while not all(mutex.locked() for mutex in exitmutexes): time.sleep(0.25)
+print('Main thread exiting.')
