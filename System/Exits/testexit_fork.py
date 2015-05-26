1c7e2c5d9f226a32531ca891ba203fe7e723e88a
zhongjiezheng <zzjie1991@gmail.com>
2015-05-27 00:02:08 +0800

    exits and pipes

---
+++ System/Exits/testexit_fork.py
@@ -0,0 +1,21 @@
+import os
+exitstat = 0
+
+def child():
+	global exitstat
+	exitstat += 1
+	print('Hello from child', os.getpid, exitstat)
+	os._exit(exitstat)
+	print('never reached')
+
+def parent():
+	while True:
+		newpid = os.fork()
+		if newpid == 0:
+			child()
+		else:
+			pid, status = os.wait()
+			print('Parent got', pid, status, (status >> 8))
+			if input() == 'q': break
+
+if __name__ == '__main__': parent()
