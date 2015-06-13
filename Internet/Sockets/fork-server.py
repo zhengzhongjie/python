9978562a4d3284547d1a365a2adaeef1c6dcf8d5
zhongjiezheng <zzjie1991@gmail.com>
2015-06-14 07:19:54 +0800

    socket

---
+++ Internet/Sockets/fork-server.py
@@ -0,0 +1,43 @@
+__author__ = 'zhongjiezheng'
+import os, time, sys
+from socket import *
+myHost = ''
+myPort = 50007
+
+sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj.bind((myHost, myPort))
+sockobj.listen(5)
+
+def now():
+	return time.ctime(time.time())
+
+activeChildren = []
+def reapChildren():
+	while activeChildren:
+		pid, stat = os.waitpid(0, os.WNOHANG)
+		if not pid: break
+		activeChildren.remove(pid)
+
+def handleClient(connection):
+	time.sleep(5)
+	while True:
+		data = connection.recv(1024)
+		if not data: break
+		reply = 'Echo=>%s at %s' % (data, now())
+		connection.send(reply.encode())
+	connection.close()
+	os._exit(0)
+
+def dispatcher():
+	while True:
+		connection, address = sockobj.accept()
+		print('Server connected by')
+		print('at', now())
+		reapChildren()
+		childPid = os.fork()
+		if childPid == 0:
+			handleClient(connection)
+		else:
+			activeChildren.append(childPid)
+
+dispatcher()
