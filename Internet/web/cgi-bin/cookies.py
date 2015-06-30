58e3af21c3797eba11e524bb61c01502d84da73e
zhongjiezheng <zzjie1991@gmail.com>
2015-06-30 23:23:22 +0800

    Server-Side Scripting

---
+++ Internet/web/cgi-bin/cookies.py
@@ -0,0 +1,15 @@
+import http.cookies, os
+cookstr  = os.environ.get("HTTP_COOKIE")
+cookies  = http.cookies.SimpleCookie(cookstr)
+usercook = cookies.get("user")
+
+if usercook == None:
+	cookies = http.cookies.SimpleCookie()
+	cookies['user'] = 'Brian'
+	print(cookies)
+	greeting = '<p>His name shall be...%s</p>' % cookies['user']
+else:
+	greeting = '<p>Welcome back, %s</p>' % usercook.value
+
+print('Content-type: text/html\n')
+print(greeting)
